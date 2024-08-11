b344d528c2554469e180d23ffaf697c7bf1d2df7
Shakil Mahmud Shuvo <62479964+ShakilMahmudShuvo@users.noreply.github.com>
2024-08-11 13:10:15 +0600

    Update routers.py

--- apis/routers.py
+++ apis/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import inference_apis
+from . import inference_apis, start_training_api
 
 api_router = APIRouter()
 
 api_router.include_router(inference_apis.router, tags=["inference"])
+api_router.include_router(start_training_api.router, tags=["training"])
